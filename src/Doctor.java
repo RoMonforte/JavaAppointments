d2ee5fb7404b3a536adb4ce62eacc9ca51eb40dd
RmC <rodrigomonforte94@gmail.com>
2023-08-04 03:28:03 -0600

    Ui Menu for the simple app

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,20 @@
 public class Doctor {
-    int id;
+    static int id = 0;
     String name;
     String speciality;
 
+    Doctor() {
+        System.out.println("Building Doctor Object");
+        id++;
+    }
+    Doctor (String name){
+        System.out.println("Doctor's Name is: " + name);
+    }
+
     public void showName() {
         System.out.println(name);
+    }
+    public void showId() {
+        System.out.println("Doctor's id: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-Doctor myDoctor = new Doctor();
-myDoctor.name = "Pedro Perez";
-myDoctor.showName();
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Pedro Perez";
+        myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        showMenu();
     }
 }
